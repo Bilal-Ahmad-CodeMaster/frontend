a0df8597ac777bf4effdfcf82adc51fceba090f0
Bilal Ahmad <bilal.codemaster@gmail.com>
2026-05-03 22:49:01 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      // This tells Turbopack to treat .ts files in edge-tts as standard TS modules
-      './node_modules/edge-tts/**/*.ts': {
-        as: '*.ts',
-      },
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
